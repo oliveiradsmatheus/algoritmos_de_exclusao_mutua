1101fdefa64729bc59069978519892b5cf0eb6f8
oliveiradsmatheus <oliveiradsmatheus@gmail.com>
2025-11-30 20:30:26 -0300

    Algoritmo de dekker para 3 processos

---
+++ dekker_3_processos/main.c
@@ -0,0 +1,85 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int *flag;
+int *turn;
+
+int main(void) {
+    int shmidT = shmget(3, sizeof(int), IPC_CREAT | 0666);
+    int shmidF = shmget(1, 3 * sizeof(int), IPC_CREAT | 0666);
+    turn = (int *) shmat(shmidT, NULL, 0);
+    flag = (int *) shmat(shmidF, NULL, 0);
+
+    *turn = 0;
+    flag[0] = flag[1] = FALSE;
+
+    if (fork() > 0) {
+        while (TRUE) {
+            flag[0] = TRUE;
+            sleep(2);
+            while (flag[1] || flag[2]) {
+                if (*turn != 0) {
+                    flag[0] = FALSE;
+                    while (*turn != 0) {
+                        printf("Estou bloqueado - processo %d\n", getpid());
+                        sleep(3);
+                    }
+                    flag[0] = TRUE;
+                }
+            }
+            printf("< Sessão crítica %d: >\n", getpid());
+            *turn = 1;
+            flag[0] = FALSE;
+        }
+    } else {
+        if (fork() > 0) {
+            while (TRUE) {
+                flag[1] = TRUE;
+                sleep(2);
+                while (flag[0] || flag[2]) {
+                    if (*turn != 1) {
+                        flag[1] = FALSE;
+                        while (*turn != 1) {
+                            printf("Estou bloqueado - processo %d\n", getpid());
+                            sleep(3);
+                        }
+                        flag[1] = TRUE;
+                    }
+                }
+                printf("< Sessão crítica %d: >\n", getpid());
+                *turn = 0;
+                flag[1] = FALSE;
+            }
+        } else {
+            while (TRUE) {
+                flag[2] = TRUE;
+                sleep(2);
+                while (flag[0] || flag[1]) {
+                    if (*turn != 1) {
+                        flag[2] = FALSE;
+                        while (*turn != 1) {
+                            printf("Estou bloqueado - processo %d\n", getpid());
+                            sleep(3);
+                        }
+                        flag[2] = TRUE;
+                    }
+                }
+                printf("< Sessão crítica %d: >\n", getpid());
+                *turn = 0;
+                flag[2] = FALSE;
+            }
+        }
+    }
+    shmdt(turn);
+    shmctl(shmidT, IPC_RMID, NULL);
+    shmdt(flag);
+    shmctl(shmidF, IPC_RMID, NULL);
+    exit(0);
+}
